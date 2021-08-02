d62eb0b3821728d89b67e667631f7cad4b4bd07c
enntomo <61280049+enntomo@users.noreply.github.com>
2021-08-03 02:18:12 +0900

    change_reply

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='退勤しました！')
-    else:
+    
 
 
 if __name__ == "__main__":
