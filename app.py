032bac319e047aeaa19594a9847dad49e8d4357b
enntomo <61280049+enntomo@users.noreply.github.com>
2021-08-03 23:39:15 +0900

    change_SP_SHEET

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def auth():
     ]
 
     SP_SHEET_KEY = '1-YncoBYoSOqfSXP_W7bAuDJ-9MdtiWk6rTCgr5oIeBc'
-    SP_SHEET = 'timesheet'
+    SP_SHEET = '遠藤'
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
     gc = gspread.authorize(credentials)
