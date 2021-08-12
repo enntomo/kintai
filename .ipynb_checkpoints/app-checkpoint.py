cf1e845e54c259ea5f5d3b8edc3628073fc27112
enntomo <61280049+enntomo@users.noreply.github.com>
2021-08-12 21:47:57 +0900

    change_order

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -19,64 +19,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def auth():
-    SP_CREDENTIAL_FILE = 'job.json'
-    SP_SCOPE = [
-        'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    SP_SHEET_KEY = '1-YncoBYoSOqfSXP_W7bAuDJ-9MdtiWk6rTCgr5oIeBc'
-    
-    if atd_name == '遠藤'
-        SP_SHEET = '遠藤'
-    elif atd_name == '長崎'
-        SP_SHEET = '長崎'
-        
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
-    gc = gspread.authorize(credentials)
-
-    worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
-    return worksheet
-
-
-
-
-
-#出勤
-def punch_in():
-    worksheet =auth()
-    df = pd.DataFrame(worksheet.get_all_records())
-    
-    timestamp = datetime.now()
-    date = timestamp.strftime('%Y/%m/%d')
-    punch_in = timestamp.strftime('%H:%M')
-
-    df = df.append({'名前': atd_name, '日付': date, '出勤時間': punch_in, '退勤時間': '00:00'}, ignore_index=True)
-    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
-
-    print('出勤完了しました！')
-#退勤
-def punch_out():
-    worksheet =auth()
-    df = pd.DataFrame(worksheet.get_all_records())
-    
-    timestamp = datetime.now()
-    punch_out = timestamp.strftime('%H:%M')
-    df.iloc[-1, 3] = punch_out
-    
-#     df_name = df[df['名前'].str.contains(atd_name)].copy()
-#     df_name.iloc[-1, 3] = punch_out
-    
-#     df_name = df[(df['遠藤'] == atd_name) & (df['退勤時間'] == '00:00')].replace({'退勤時間': {'00:00': punch_out}})
-#     df_name = df[(df['名前'] == atd_name) & (df['退勤時間'] == '00:00')].replace({'退勤時間': {'00:00': punch_out}})
-    
-    
-    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
-    print('退勤しました！')
-
-
-        
-
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = 'xaD08TJKexbs26kL74LkJWmkCYPbhI+FusQz9Nt2sl4tFxxSeHRNUf2Y2/Y8dUpC896jf216rsAgA0y5A8oiITm8Ze7LLtGZ8/IRhHHYL/k3+jepuf28lDMlwFEmkrb/v3at7UFRgHeiZZ2Rw69iSQdB04t89/1O/w1cDnyilFU='
@@ -135,7 +77,70 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
     
     
+#出勤
+def punch_in():
+    worksheet =auth()
+    df = pd.DataFrame(worksheet.get_all_records())
     
+    timestamp = datetime.now()
+    date = timestamp.strftime('%Y/%m/%d')
+    punch_in = timestamp.strftime('%H:%M')
+
+    df = df.append({'名前': atd_name, '日付': date, '出勤時間': punch_in, '退勤時間': '00:00'}, ignore_index=True)
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+    print('出勤完了しました！')
+#退勤
+def punch_out():
+    worksheet =auth()
+    df = pd.DataFrame(worksheet.get_all_records())
+    
+    timestamp = datetime.now()
+    punch_out = timestamp.strftime('%H:%M')
+    df.iloc[-1, 3] = punch_out
+    
+#     df_name = df[df['名前'].str.contains(atd_name)].copy()
+#     df_name.iloc[-1, 3] = punch_out
+    
+#     df_name = df[(df['遠藤'] == atd_name) & (df['退勤時間'] == '00:00')].replace({'退勤時間': {'00:00': punch_out}})
+#     df_name = df[(df['名前'] == atd_name) & (df['退勤時間'] == '00:00')].replace({'退勤時間': {'00:00': punch_out}})
+    
+    
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    print('退勤しました！')
+    
+    
+    
+
+def auth():
+    SP_CREDENTIAL_FILE = 'job.json'
+    SP_SCOPE = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/drive'
+    ]
+    SP_SHEET_KEY = '1-YncoBYoSOqfSXP_W7bAuDJ-9MdtiWk6rTCgr5oIeBc'
+    
+    if atd_name == '遠藤'
+        SP_SHEET = '遠藤'
+    elif atd_name == '長崎'
+        SP_SHEET = '長崎'
+        
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
+    gc = gspread.authorize(credentials)
+
+    worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
+    return worksheet
+
+
+
+
+
+
+
+
+        
+
+
 
 
 
