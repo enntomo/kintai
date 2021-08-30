7a8f0f9ddd903b6ab22cb36bc975c967d1f2babc
enntomo <61280049+enntomo@users.noreply.github.com>
2021-08-30 20:46:40 +0900

    test

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -29,20 +29,9 @@ def auth():
     SP_SHEET_KEY = '1-YncoBYoSOqfSXP_W7bAuDJ-9MdtiWk6rTCgr5oIeBc'
     
     global SP_SHEET
-    
-    if name != SP_SHEET:
-            worksheet = gc.open_by_key(SP_SHEET_KEY).add_worksheet(title=atd_name, rows=100, cols=20)
-            datas = [
-              ['名前', '日付', '出勤時間', '退勤時間'],
-            ]
+    SP_SHEET = atd_name
 
-            for row_data in datas:
-                worksheet.append_row(row_data)
-    else:
-        SP_SHEET = atd_name
-        worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
-
-    
+        
         
     credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
     gc = gspread.authorize(credentials)
